82b7821dc2d07202bc09bc65c6e1cacf1b3556b5
kathrynhoule <kathryn.houle@torontomu.ca>
2024-12-03 18:20:43 -0500

    loads player sprites and adds animation for walking around the screen

--- Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/GalvanyzeGame.java
+++ Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/GalvanyzeGame.java
@@ -1,32 +1,86 @@
 package ca.grimlineage.galvanyze;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GalvanyzeGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+     private SpriteBatch batch;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+     private Texture playerSheet;
+     private TextureRegion[] walkFrames;
+     private Animation<TextureRegion> walkAnimation;
+     private float animationTime;
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+     private float playerX, playerY;
+     private float speed = 80;
+ 
+     @Override
+     public void create() {
+         batch = new SpriteBatch();
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+         playerSheet = new Texture("ghost_walk_front.png");
+        
+          TextureRegion[][] tmpFrames = TextureRegion.split(playerSheet, 
+               playerSheet.getWidth() / 4, playerSheet.getHeight());
+          walkFrames = new TextureRegion[4];
+          for (int i = 0; i < 4; i++) {
+               walkFrames[i] = tmpFrames[0][i];
+          }
+
+          walkAnimation = new Animation<>(0.1f, walkFrames);
+          animationTime = 0f;
+
+          playerX = Gdx.graphics.getWidth() / 2f - walkFrames[0].getRegionWidth() / 2f;
+          playerY = Gdx.graphics.getHeight() / 2f - walkFrames[0].getRegionHeight() / 2f;
+     }
+ 
+     @Override
+     public void render() {
+
+        Gdx.gl.glClearColor(0, 0, 0, 1); 
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
+
+
+        animationTime += Gdx.graphics.getDeltaTime();
+ 
+         float deltaTime = Gdx.graphics.getDeltaTime();
+         boolean isWalking = false;
+         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+             playerY += speed * deltaTime;
+             isWalking = true;
+         }
+         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+             playerY -= speed * deltaTime;
+             isWalking = true;
+         }
+         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+             playerX -= speed * deltaTime;
+             isWalking = true;
+         }
+         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+             playerX += speed * deltaTime;
+             isWalking = true;
+         }
+ 
+         TextureRegion currentFrame = walkFrames[0]; 
+         if (isWalking) {
+             currentFrame = walkAnimation.getKeyFrame(animationTime, true);
+         }
+ 
+         batch.begin();
+         batch.draw(currentFrame, playerX, playerY);
+         batch.end();
+     }
+ 
+     @Override
+     public void dispose() {
+         batch.dispose();
+         playerSheet.dispose();
+     }
 }
