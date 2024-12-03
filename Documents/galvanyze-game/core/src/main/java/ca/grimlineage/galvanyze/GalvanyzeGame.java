c8f1db6d59b5d3317331ea213ef27a5a23f594a0
kathrynhoule <kathryn.houle@torontomu.ca>
2024-12-03 18:26:36 -0500

    moves some logic over to player file

--- Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/GalvanyzeGame.java
+++ Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/GalvanyzeGame.java
@@ -2,85 +2,36 @@ package ca.grimlineage.galvanyze;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GalvanyzeGame extends ApplicationAdapter {
-     private SpriteBatch batch;
+    private SpriteBatch batch;
+    private Player player;
 
-     private Texture playerSheet;
-     private TextureRegion[] walkFrames;
-     private Animation<TextureRegion> walkAnimation;
-     private float animationTime;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        player = new Player("ghost_walk_front.png", Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f);
+    }
 
-     private float playerX, playerY;
-     private float speed = 80;
- 
-     @Override
-     public void create() {
-         batch = new SpriteBatch();
-
-         playerSheet = new Texture("ghost_walk_front.png");
-        
-          TextureRegion[][] tmpFrames = TextureRegion.split(playerSheet, 
-               playerSheet.getWidth() / 4, playerSheet.getHeight());
-          walkFrames = new TextureRegion[4];
-          for (int i = 0; i < 4; i++) {
-               walkFrames[i] = tmpFrames[0][i];
-          }
-
-          walkAnimation = new Animation<>(0.1f, walkFrames);
-          animationTime = 0f;
-
-          playerX = Gdx.graphics.getWidth() / 2f - walkFrames[0].getRegionWidth() / 2f;
-          playerY = Gdx.graphics.getHeight() / 2f - walkFrames[0].getRegionHeight() / 2f;
-     }
- 
-     @Override
-     public void render() {
-
+    @Override
+    public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1); 
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        
+        player.update(deltaTime);
+        
+        batch.begin();
+        player.render(batch);
+        batch.end();
+    }
 
-        animationTime += Gdx.graphics.getDeltaTime();
- 
-         float deltaTime = Gdx.graphics.getDeltaTime();
-         boolean isWalking = false;
-         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-             playerY += speed * deltaTime;
-             isWalking = true;
-         }
-         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-             playerY -= speed * deltaTime;
-             isWalking = true;
-         }
-         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-             playerX -= speed * deltaTime;
-             isWalking = true;
-         }
-         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-             playerX += speed * deltaTime;
-             isWalking = true;
-         }
- 
-         TextureRegion currentFrame = walkFrames[0]; 
-         if (isWalking) {
-             currentFrame = walkAnimation.getKeyFrame(animationTime, true);
-         }
- 
-         batch.begin();
-         batch.draw(currentFrame, playerX, playerY);
-         batch.end();
-     }
- 
-     @Override
-     public void dispose() {
-         batch.dispose();
-         playerSheet.dispose();
-     }
+    @Override
+    public void dispose() {
+        batch.dispose();
+        player.dispose();
+    }
 }
--- Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/Player.java
+++ Documents/galvanyze-game/core/src/main/java/ca/grimlineage/galvanyze/Player.java
@@ -1,5 +1,58 @@
 package ca.grimlineage.galvanyze;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Player {
-     
+    private Texture playerSheet;
+    private TextureRegion[] walkFrames;
+    private Animation<TextureRegion> walkAnimation;
+    private float animationTime;
+    private float x, y;
+    private float speed = 80;
+
+    public Player(String texturePath, float initialX, float initialY) {
+        playerSheet = new Texture("ghost_walk_front.png");
+        
+        // split sprite sheet into frames
+        TextureRegion[][] tmpFrames = TextureRegion.split(playerSheet, playerSheet.getWidth() / 4, playerSheet.getHeight());
+        walkFrames = new TextureRegion[4];
+        for (int i = 0; i < 4; i++) {
+            walkFrames[i] = tmpFrames[0][i];
+        }
+
+        walkAnimation = new Animation<>(0.1f, walkFrames);
+        animationTime = 0f;
+
+        x = initialX;
+        y = initialY;
+    }
+
+    public void update(float deltaTime) {
+        animationTime += deltaTime;
+        
+        // movement keys
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.W)) y += speed * deltaTime;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.S)) y -= speed * deltaTime;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.A)) x -= speed * deltaTime;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.D)) x += speed * deltaTime;
+    }
+
+    public void render(SpriteBatch batch) {
+
+        TextureRegion currentFrame = walkFrames[0];
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.W) || Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.S) || 
+            Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.A) || Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.D)) {
+            currentFrame = walkAnimation.getKeyFrame(animationTime, true);
+        }
+        
+        batch.draw(currentFrame, x, y);
+    }
+
+    public void dispose() {
+        playerSheet.dispose();
+    }
 }
